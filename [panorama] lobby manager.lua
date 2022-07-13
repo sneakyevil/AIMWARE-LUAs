6efec6458ed7edbca4dccb8125aaa043c8417ae2
sneakyevil <sneakyevildogs@gmail.com>
2022-07-13 17:07:21 +0200

    Update [panorama] lobby manager.lua

--- [panorama] lobby manager.lua
+++ [panorama] lobby manager.lua
@@ -1,150 +1,161 @@
--- CommandHandle
-panorama.RunScript([[
+local function Register_HandleChatCommand()
+    panorama.RunScript([[
 
-    var m_gResponseChat = function(msg)
-    {
-        msg = msg.split(' ').join('\u{00A0}')
-        PartyListAPI.SessionCommand('Game::Chat', 'run all xuid ' + MyPersonaAPI.GetXuid() + ' chat ->\u2029' + msg);
-    }
-
-    var m_gFindPlayer = function(str)
-    {
-        // Name
-        for (var i=0; PartyListAPI.GetCount() > i; ++i) 
+        var m_gResponseChat = function(msg)
         {
-            var xuid        = PartyListAPI.GetXuidByIndex(i);
-            var username    = PartyListAPI.GetFriendName(xuid);
-            if (username.toLowerCase().indexOf(str) != -1) 
-                return username;
+            msg = msg.split(' ').join('\u{00A0}')
+            PartyListAPI.SessionCommand('Game::Chat', 'run all xuid ' + MyPersonaAPI.GetXuid() + ' chat ->\u2029' + msg);
         }
 
-        // Index
-        for (var i=0; PartyListAPI.GetCount() > i; ++i) 
+        var m_gFindPlayer = function(str)
         {
-            if (parseInt(str) == i) 
-                return PartyListAPI.GetFriendName(PartyListAPI.GetXuidByIndex(i));
-        }
-
-        return '';
-    }
-
-    var m_gHandleChatCommand = function(username, msg)
-    {
-        var args = msg.toLowerCase().split(' ');
-        var target = (args.length > 1 ? m_gFindPlayer(args[1]) : username)
-        if (target.length == 0) target = username;
-
-        if (args[0] == '!help' || args[0] == '!cmds')
-        {
-            var cmds = [ 
-                '!iq <partial:name>|<lobbyindex>', '!dick <partial:name>|<lobbyindex>', '!gay <partial:name>|<lobbyindex>',
-                '!loc <partial:name>',
-                '!startq (start queue)', '!stopq (stop queue)', 
-                '!norris (chuck norris joke)',
-            ];
-
-            msg = '\u2029';
-            for (var i = 0; cmds.length > i; ++i)
-                msg += cmds[i] + '\u2029';
-
-            return m_gResponseChat(msg);
-        }
-
-        if (args[0] == '!iq')
-            return m_gResponseChat('IQ of ' + target + ' is ' +  Math.floor(Math.random() * 420) + '.');
-
-        if (args[0] == '!dick')
-            return m_gResponseChat('Dick size of ' + target + ' is ' +  Math.floor(Math.random() * 40) + ' cm.');
-
-        if (args[0] == '!gay')
-            return m_gResponseChat(target + ' is ' +  Math.floor(Math.random() * 101) + '% gay.');
-            
-        if (args[0] == '!loc')
-        {
-            msg = '';
-
-            var settings = LobbyAPI.GetSessionSettings();
-			for (var i = 0; settings.members.numMachines > i; ++i) 
+            // Name
+            for (var i=0; PartyListAPI.GetCount() > i; ++i) 
             {
-				var player = settings.members[`machine${i}`];
+                var xuid        = PartyListAPI.GetXuidByIndex(i);
+                var username    = PartyListAPI.GetFriendName(xuid);
+                if (username.toLowerCase().indexOf(str) != -1) 
+                    return username;
+            }
 
-                if (args.length > 1)
+            // Index
+            for (var i=0; PartyListAPI.GetCount() > i; ++i) 
+            {
+                if (parseInt(str) == i) 
+                    return PartyListAPI.GetFriendName(PartyListAPI.GetXuidByIndex(i));
+            }
+
+            return '';
+        }
+
+        var m_gHandleChatCommand = function(username, msg)
+        {
+            var args = msg.toLowerCase().split(' ');
+            var target = (args.length > 1 ? m_gFindPlayer(args[1]) : username)
+            if (target.length == 0) target = username;
+
+            if (args[0] == '!help' || args[0] == '!cmds')
+            {
+                var cmds = [ 
+                    '!iq <partial:name>|<lobbyindex>', '!dick <partial:name>|<lobbyindex>', '!gay <partial:name>|<lobbyindex>',
+                    '!loc <partial:name>',
+                    '!startq (start queue)', '!stopq (stop queue)', 
+                    '!norris (chuck norris joke)',
+                ];
+
+                msg = '\u2029';
+                for (var i = 0; cmds.length > i; ++i)
+                    msg += cmds[i] + '\u2029';
+
+                return m_gResponseChat(msg);
+            }
+
+            if (args[0] == '!iq')
+                return m_gResponseChat('IQ of ' + target + ' is ' +  Math.floor(Math.random() * 420) + '.');
+
+            if (args[0] == '!dick')
+                return m_gResponseChat('Dick size of ' + target + ' is ' +  Math.floor(Math.random() * 40) + ' cm.');
+
+            if (args[0] == '!gay')
+                return m_gResponseChat(target + ' is ' +  Math.floor(Math.random() * 101) + '% gay.');
+                
+            if (args[0] == '!loc')
+            {
+                msg = '';
+
+                var settings = LobbyAPI.GetSessionSettings();
+                for (var i = 0; settings.members.numMachines > i; ++i) 
                 {
-                    if (player.player0.name.toLowerCase().indexOf(target) != -1)
+                    var player = settings.members[`machine${i}`];
+
+                    if (args.length > 1)
                     {
-                        msg = player.player0.name + ' is from ' + player.player0.game.loc;
-                        break;
+                        if (player.player0.name.toLowerCase().indexOf(target) != -1)
+                        {
+                            msg = player.player0.name + ' is from ' + player.player0.game.loc;
+                            break;
+                        }
+                    }
+                    else
+                        msg += player.player0.name + ' is from ' + player.player0.game.loc + '\u2029';
+                }
+
+                return m_gResponseChat(msg);
+            }
+
+            if (args[0] == '!startq')
+                return LobbyAPI.StartMatchmaking( '', '', '', '' );
+
+            if (args[0] == '!stopq')
+                return LobbyAPI.StopMatchmaking();
+
+            if (args[0] == '!norris')
+            {
+                $.AsyncWebRequest('https://api.chucknorris.io/jokes/random', 
+                { 
+                    type: "GET", complete:function(e) 
+                    {
+                        if (e.status == 200) 
+                            m_gResponseChat(JSON.parse(e.responseText.substring(0, e.responseText.length - 1)).value)
+                    }
+                });
+                return;
+            }
+        }
+
+    ]])
+end
+
+local function Register_HandleChatMessage()
+    panorama.RunScript([[
+
+        var m_gHandleChatMessage = function(should_handle)
+        { 
+            var m_gPartyChat = $.GetContextPanel().FindChildTraverse("PartyChat")
+            if (!m_gPartyChat) return;
+            
+            var m_gChatLines = m_gPartyChat.FindChildTraverse("ChatLinesContainer")
+            if (!m_gChatLines) return;
+            
+            m_gChatLines.Children().forEach(el => 
+            {
+                var child = el.GetChild(0)
+                if (child && child.BHasClass('left-right-flow') && child.BHasClass('horizontal-align-left')) 
+                {
+                    if (!child.BHasClass('aw_handled')) 
+                    {
+                        child.AddClass('aw_handled');
+
+                        try 
+                        {
+                            var InnerChild = child.GetChild(child.GetChildCount() - 1);
+                            if (InnerChild && InnerChild.text) 
+                            {
+                                var Sender = $.Localize('{s:player_name}', InnerChild);
+                                var Message = $.Localize('{s:msg}', InnerChild);
+                                if (should_handle && Message[0] == '!')
+                                    m_gHandleChatCommand(Sender, Message);
+                            }
+                        } 
+                        catch(err) { }
                     }
                 }
-                else
-                    msg += player.player0.name + ' is from ' + player.player0.game.loc + '\u2029';
-            }
-
-            return m_gResponseChat(msg);
+            })
         }
 
-        if (args[0] == '!startq')
-            return LobbyAPI.StartMatchmaking( '', '', '', '' );
+        // set old msgs as handled
+        m_gHandleChatMessage(false);
+    ]])
+end
 
-        if (args[0] == '!stopq')
-            return LobbyAPI.StopMatchmaking();
+-- Functions
+local function IsInGame()
+    if (entities.GetByIndex(0) == nil) then 
+        return false 
+    end
 
-        if (args[0] == '!norris')
-        {
-            $.AsyncWebRequest('https://api.chucknorris.io/jokes/random', 
-            { 
-                type: "GET", complete:function(e) 
-                {
-					if (e.status == 200) 
-                        m_gResponseChat(JSON.parse(e.responseText.substring(0, e.responseText.length - 1)).value)
-				}
-			});
-            return;
-        }
-    }
-
-]])
-
--- HandleChatMessage
-panorama.RunScript([[
-
-    var m_gHandleChatMessage = function(should_handle)
-    { 
-        var m_gPartyChat = $.GetContextPanel().FindChildTraverse("PartyChat")
-        if (!m_gPartyChat) return;
-        
-        var m_gChatLines = m_gPartyChat.FindChildTraverse("ChatLinesContainer")
-        if (!m_gChatLines) return;
-        
-        m_gChatLines.Children().forEach(el => 
-        {
-            var child = el.GetChild(0)
-            if (child && child.BHasClass('left-right-flow') && child.BHasClass('horizontal-align-left')) 
-            {
-                if (!child.BHasClass('aw_handled')) 
-                {
-                    child.AddClass('aw_handled');
-
-                    try 
-                    {
-                        var InnerChild = child.GetChild(child.GetChildCount() - 1);
-                        if (InnerChild && InnerChild.text) 
-                        {
-                            var Sender = $.Localize('{s:player_name}', InnerChild);
-                            var Message = $.Localize('{s:msg}', InnerChild);
-                            if (should_handle && Message[0] == '!')
-                                m_gHandleChatCommand(Sender, Message);
-                        }
-                    } 
-                    catch(err) { }
-                }
-            }
-        })
-    }
-
-    // set old msgs as handled
-    m_gHandleChatMessage(false);
-]])
+    return true
+end
 
 -- Variables
 local m_NextChatMessageCheck = 0.0
@@ -156,6 +167,7 @@ local m_Options     = gui.Groupbox(m_Tab, "Options", 16, 16, 300)
 local m_AutoAccept          = gui.Checkbox(m_Options, "lobbymgr_autoaccept", "Auto-Accept", true)
 local m_AutoAcceptValue     = false
 local m_Commands            = gui.Checkbox(m_Options, "lobbymgr_commands", "Commands", true)
+gui.Button(m_Options, "Create Session", function() panorama.RunScript("FriendsListAPI.ActionInviteFriend('0', '');") end)
 
 local m_Troll       = gui.Groupbox(m_Tab, "Troll", 300 + 16 + 5, 16, 300)
 local m_PlayerIndex = gui.Slider(m_Troll, "lobbymgr_playerindex", "Player Index (Host - 0)", 0, 0, 4)
@@ -182,8 +194,21 @@ gui.Button(m_Troll, "Close Popups", function()
     panorama.RunScript("UiToolkitAPI.CloseAllVisiblePopups();")
 end)
 
+local m_RegisterHandles = true
 callbacks.Register("Draw", function()
-    if (globals.MaxClients() ~= 0) then return end
+    if (IsInGame()) then 
+        m_RegisterHandles = true
+        return 
+    end
+
+    if (m_RegisterHandles) then
+        m_RegisterHandles = false
+
+        Register_HandleChatCommand()
+        Register_HandleChatMessage()
+
+        m_AutoAcceptValue = (not m_AutoAccept:GetValue())
+    end
     
     local m_RealTime = globals.RealTime()
     if (m_RealTime > m_NextChatMessageCheck) then
@@ -197,7 +222,15 @@ callbacks.Register("Draw", function()
         if (m_AutoAcceptValue) then
             panorama.RunScript([[
 
-                var m_gHandleAccept = function() { $.DispatchEvent('MatchAssistedAccept'); }
+                var m_gHandleAccept = function() 
+                {
+                    if (typeof(m_gAcceptTime) == 'undefined' || Date.now() - m_gAcceptTime > 1500)
+                    {
+                        m_gAcceptTime = Date.now()
+                        $.DispatchEvent('MatchAssistedAccept');
+                    }
+                }
+
                 $.RegisterForUnhandledEvent('PanoramaComponent_Lobby_ReadyUpForMatch', m_gHandleAccept);
 
             ]])
